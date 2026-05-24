f6ccd83e4649ba4231e8c3847cc965b5ab0cf232
HlobisileZulu <hlobiemageba@gmail.com>
2026-05-24 23:39:01 +0200

    Delete IntroToUnitTesting directory

--- IntroToUnitTesting/src/test/java/DemoTest.java
+++
@@ -1,9 +0,0 @@
-/**
- * The class containing your tests for the {@link Demo} class.  Make sure you
- * test all methods in this class (including both 
- * {@link Demo#main(String[])} and 
- * {@link Demo#isTriangle(double, double, double)}).
- */
-public class DemoTest {
-
-}
